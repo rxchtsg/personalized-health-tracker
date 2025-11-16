681bfca738b0c2a39248ee85bc8393a818b4a5d9
Rachel Tsang <rachel.tsang@code.berlin>
2025-11-16 13:49:12 +0100

    add select queries for habits

--- backend/db.js
+++ backend/db.js
@@ -4,3 +4,15 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
+function allHabits() {
+  const sql = 'SELECT * FROM habits ORDER BY id DESC';
+  return pool.query(sql).then(result => result.rows);
+}
+function getHabit(id) {
+  const sql = 'SELECT * FROM habits WHERE id = $1';
+  return pool.query(sql, [id]).then(result => result.rows[0]);
+}
+module.exports = {
+  allHabits,
+  getHabit
+};
